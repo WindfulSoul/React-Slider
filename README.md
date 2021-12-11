2052ea911f488e3ddadcdd6a62215539c954fa1c
Song Aiden Ko <81278660+WindfulSoul@users.noreply.github.com>
2021-12-10 20:33:08 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# React-Slider-Practice
-# React useState and useEffect
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      name: 'Nichole Lively',
+      title: 'Customer',
+      quote:
+        'Rose Canyon and Dr. Michael are the best!  Never does unnecessary tests and is always so loving to my Mocha (she is an 8 year old Mini-Dachshund).',
+    },
+    {
+      id: 2,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      name: 'Kevin Grant',
+      title: 'Customer',
+      quote:
+        'This place is absolutely amazing!  They love my dog as much as I do and it shows in the level of care they provide to everyone and every animal.',
+    },
+    {
+      id: 3,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+      name: 'Andy Just',
+      title: 'Royal Customer',
+      quote:
+        'I have been a client of Rose Canyon Animal Hospital since the day it opened and have always had the very best caring and compassionate service.',
+    },
+    {
+      id: 4,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      name: 'Jennifer Lovehill',
+      title: 'Customer',
+      quote:
+        'I appreciate the precautions they are taking to make sure everyone, humans & pets, are as safe and healthy as possible.',
+    },
+  ];
+  
+  export default people;
